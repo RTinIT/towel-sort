17272766dd9b0dc9621c71efebb1ad274c994a9d
rtinit <fizrukaleksikov@gmail.com>
2022-02-26 21:19:16 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
-// You should implement your task here.
+
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined || matrix.length === 0) {
+    return []
+  }
+  let res = []
+  matrix.forEach((elem, index) => comparision(elem, index))
+  function comparision(elem, index) {
+      if (index % 2 !== 0) {
+          elem = elem.reverse() 
+          res.push(elem)
+      } else {
+          res.push(elem)
+      }
+  }
+  res = [].concat.apply([], res)
+  return res
 }
